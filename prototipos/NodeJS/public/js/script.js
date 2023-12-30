6de25ac4b298fec15560bf37a29da4303a6ebb77
YamidHRodriguez <horaciohabbos@gmail.com>
2023-12-29 23:25:36 -0500

    Modifación: Funcionalidad del Toggle del home

--- prototipos/NodeJS/public/js/script.js
+++ prototipos/NodeJS/public/js/script.js
@@ -1,7 +1,7 @@
 
 // Aquí se agregará la fecha y hora actual
 var fechaYHoraElemento = document.getElementById("fechaYHora");
-    
+
 // Función para actualizar la fecha y hora
 function actualizarFechaYHora() {
     var fechaHoraActual = new Date();
@@ -18,24 +18,40 @@ actualizarFechaYHora();
 function menu_desplegable() {
     var menu = document.getElementById("menu_desplegable");
     var fondo = document.getElementById("fondo_oscuro");
-    if (menu.style.display === "none" && fondo.style.display === "none") {
+    var circle_opc = document.getElementById("circle_opc");
+    var menuDos = document.getElementById("menu_desplegable_2");
+
+    if ((menu.style.display === "none" && fondo.style.display === "none" && menuDos.style.display==="none")){
         fondo.style.display = "block";
         fondo.style.position = "fixed";
         menu.style.position = "absolute";
         menu.style.display = "flex";
+        circle_opc.style.display = "none";
+  
     } else {
         menu.style.display = "none";
         fondo.style.display = "none";
+        circle_opc.style.display = "flex";
+        menuDos.style.display = "none";
     }
+    
 }
 
 // Función para cerrar el menú desplegable
 function cerrar_menu() {
     var menu = document.getElementById("menu_desplegable");
     var fondo = document.getElementById("fondo_oscuro");
+    var circle_opc = document.getElementById("circle_opc");
     menu.style.display = "none";
+    circle_opc.style.display = "flex";
     var fondo = document.getElementById("fondo_oscuro");
     fondo.style.display = "none";
+    menuDos.style.display = "flex";
+}
+
+function toggleMenu() {
+    var menu = document.getElementById("menu_desplegable_2");
+    menu.style.display = (menu.style.display === "none" || menu.style.display === "") ? "flex" : "none";
 }
 
 
